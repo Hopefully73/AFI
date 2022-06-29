63cfa7e570abe728fafdcd69eb12f5adf51da97e
unknown <gwyn.magoncia73@gmail.com>
2022-06-29 15:09:25 +0800

    Added google analytics code

--- index.py
+++ index.py
@@ -6,3 +6,30 @@ app.title = "Alien Food Invasion"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5N92Y8P166"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5N92Y8P166');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
